b9441d8f1142c1a8e30296bcc2411fd266b7cceb
Enrico Penta <e.penta@logicainformatica.onmicrosoft.com>
2021-04-27 10:51:40 +0200

    Working On Events

--- LoGiocaApp/LoGioca/www/js/votes.js
+++ LoGiocaApp/LoGioca/www/js/votes.js
@@ -23,11 +23,40 @@
  };
  
  
+app.votes.votesAfterIn = function () {
+    app.votes.prepareGrid();
+};
  
- 
- app.votes.votesAfterIn = function(){
-     
- }
+app.votes.prepareGrid = function () {
+    // COSTRUZIONE SIMULAZIONE DATI
+    j = 0;
+    team = ["pippo", "pluto", "paperino", "tronchi"];
+    teamTwo = ["luca", "peppe", "mario", "luigi"];
+    //
+    for (i = 0; i < team.length; i++) {
+        rec = '<div class="gridTableVotes">';
+        rec += '<div onclick="app.votes.votePage(' + j + ',\'' + team[i]+'\')" class="colVotesStyle">' + team[i] + '</div>';
+        j++;
+        rec += '<div onclick="app.votes.votePage(' + j + ',\'' + teamTwo[i] +'\')" class="colVotesStyle">' + teamTwo[i] + '</div>';
+        j++;
+        rec += '</div>';
+        $('#playersGrid').append(rec);
+    }
+
+};
+
+app.votes.votePage = function (id,playerInfo) {
+    $('.upperDiv').show();
+    $('.eventsBox').hide();
+    rec =  '<div style="font-size: -webkit-xxx-large;">'+playerInfo+'</div>';
+    rec += '<div style="display:flex; justify-content:center;">';
+    rec += '<h1>VOTO:</h1>';
+    rec += '<select class="voteSelect">';
+        for(i=1;i<=10;i++)
+            rec += '<option value='+i+'>'+i+'</option>';
+    rec += '</select></div>';
+    $('#votePart').html(rec);
+};
  
  
  
