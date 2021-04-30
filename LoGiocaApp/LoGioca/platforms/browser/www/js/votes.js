1d99f955e5e072b2be20e1789b6d87c9bc75c3a5
Enrico Penta <e.penta@logicainformatica.onmicrosoft.com>
2021-04-30 11:03:19 +0200

    Commit working on VOtes

--- LoGiocaApp/LoGioca/platforms/browser/www/js/votes.js
+++ LoGiocaApp/LoGioca/platforms/browser/www/js/votes.js
@@ -2,32 +2,32 @@
  * Gestione delle funzioni relative al login/logout
  */
 
- app = app || {}; // per evitare warning nell'editor IDE
- util = util || {}; // per evitare warning nell'editor IDE
- app.votes = {};
- 
- /*
- * prepareLogin: Definisce l'altezza della pagina e carica il template
- */
- app.votes.prepareVotes = function(routeTo, routeFrom, resolve, reject) {
-     
-     resolve({
-         templateUrl: './html/app_votes.html?7'
-     }, {
-         context: {
-             
-         }
-     });
- 
- 
- };
- 
- 
+app = app || {}; // per evitare warning nell'editor IDE
+util = util || {}; // per evitare warning nell'editor IDE
+app.votes = {};
+
+/*
+* prepareLogin: Definisce l'altezza della pagina e carica il template
+*/
+app.votes.prepareVotes = function (routeTo, routeFrom, resolve, reject) {
+
+    resolve({
+        templateUrl: './html/app_votes.html?7'
+    }, {
+        context: {
+
+        }
+    });
+
+
+};
+
+
 app.votes.votesAfterIn = function () {
     app.votes.prepareGrid();
 };
- 
-app.votes.prepareGrid = function () {
+
+app.votes.prepareGrid = function (teams) {
     // COSTRUZIONE SIMULAZIONE DATI
     j = 0;
     team = ["Enrico Penta", "Marco Merrino", "Francesco Orsini", "Federico Tronchi"];
@@ -35,9 +35,9 @@ app.votes.prepareGrid = function () {
     //
     for (i = 0; i < team.length; i++) {
         rec = '<div class="gridTableVotes">';
-        rec += '<div onclick="app.votes.votePage(' + j + ',\'' + team[i]+'\')" class="colVotesStyle">' + team[i] + '</div>';
+        rec += '<div onclick="app.votes.votePage(' + j + ',\'' + team[i] + '\')" class="colVotesStyle">' + team[i] + '</div>';
         j++;
-        rec += '<div onclick="app.votes.votePage(' + j + ',\'' + teamTwo[i] +'\')" class="colVotesStyle">' + teamTwo[i] + '</div>';
+        rec += '<div onclick="app.votes.votePage(' + j + ',\'' + teamTwo[i] + '\')" class="colVotesStyle">' + teamTwo[i] + '</div>';
         j++;
         rec += '</div>';
         $('#playersGrid').append(rec);
@@ -45,15 +45,17 @@ app.votes.prepareGrid = function () {
 
 };
 
-app.votes.votePage = function (id,playerInfo) {
+
+
+app.votes.votePage = function (id, playerInfo) {
     $('.upperDiv').show();
     $('.eventsBox').hide();
-    rec =  '<label class="labelFontPlayer">'+playerInfo+'</label>';
+    rec = '<label class="labelFontPlayer">' + playerInfo + '</label>';
     rec += '<div class="votesSelectDiv">';
     rec += '<label class="labelFont">VOTO:</label>';
     rec += '<select class="voteSelect">';
-        for(i=config.minVote;i<=config.maxVote;i++)
-            rec += '<option value='+i+'>'+i+'</option>';
+    for (i = config.minVote; i <= config.maxVote; i++)
+        rec += '<option value=' + i + '>' + i + '</option>';
     rec += '</select></div>';
     rec += '<div class="votesCommentBox">';
     rec += '<label class="labelFont">Commento:</label>';
@@ -62,15 +64,14 @@ app.votes.votePage = function (id,playerInfo) {
     rec += '</div >';
     $('#votePart').html(rec);
 };
- 
- 
- 
+
+
 //  app.votes.doLogin = function(){
-     
+
     //  var email=document.getElementById("idEmail").value;
     //  var password=document.getElementById("idPwd").value;
-     
-     
+
+
     //  util.callServer({
     //              func: 'Register/Login',
     //              value: {
@@ -79,26 +80,26 @@ app.votes.votePage = function (id,playerInfo) {
     //              }
     //          },
     //          function(success, response) {
-                 
+
     //                  app.goto("/Home/");
-                 
+
     //          }
     //  );
-     
-     /*$.ajax({
-                     type: 'POST',
-                     url: "https://logiocarest.azurewebsites.net/Register/Login",
-                     data:
-                     {
-                         dataS: '2021-04-1',
-                         postazione: 16,
-                         appartamento: 'B4',
-                     },
-                     type: 'application/json',
-                     success: function(re){ alert(JSON.stringify(re));}
-                 });*/
-     
-     
-     
-     
+
+/*$.ajax({
+                type: 'POST',
+                url: "https://logiocarest.azurewebsites.net/Register/Login",
+                data:
+                {
+                    dataS: '2021-04-1',
+                    postazione: 16,
+                    appartamento: 'B4',
+                },
+                type: 'application/json',
+                success: function(re){ alert(JSON.stringify(re));}
+            });*/
+
+
+
+
 //  }
